864b48c34a1d1b655233568ed73b367abf61aa09
Eva Veskova Jackson <51458322+evaveskova@users.noreply.github.com>
2020-03-24 11:58:03 +0100

    add footer and header

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-// import style from './style.module.css';
+import style from './style.module.css';
+import { radish } from '../../Assets/images';
 
 const Footer = () => (
-  <div />
+  <div className={style.footerWrapper}>
+    <img
+      src={radish}
+      alt="Radish Spirit"
+      className={style.footerImg}
+      height="70vh"
+    />
+    <div className={style.footerText}>
+      <p>
+        Built by
+        <a href="https://github.com/evaveskova">Eva Veskova Jackson</a>
+        .
+      </p>
+      <p>
+        For more information on Studio Ghibli, visit their
+        <a href="http://www.ghibli.jp/">official website</a>
+        .
+      </p>
+    </div>
+  </div>
 );
 
 export default Footer;
